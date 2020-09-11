8e6a1eea0740bfc77bb9192c9140068e91a8e4b7
SplitScreenStudios <60277536+SplitScreenStudios@users.noreply.github.com>
2020-09-11 14:48:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DrScriptastic", # Replace with your own username
+    name="LinkCrypt", # Replace with your own username
     version="0.0.1",
     author="DrScriptastic",
     author_email="codeusstudio@gmail.com",
